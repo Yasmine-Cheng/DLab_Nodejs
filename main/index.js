9544ef859060bc56edfa6d1cb94c382ff30c1bbf
Yasmine <08170120@gm.scu.edu.tw>
2021-08-02 10:40:01 +0800

    Add files via upload

--- main/index.js
+++ main/index.js
@@ -1,38 +1,20 @@
-// require("dotenv").config();
-// const express=require("express");
-// const app = express();
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./src/routes/user.route");
 const articleRoutes = require('./src/routes/article.route');
-// const port = process.env.PORT || 3001;
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-// const userRouter = require("./src/routes/user.route");
-
-
-
-// //define root route
-// app.get('/',(req,res) => {
-//   res.status(200).send('WELCOME TO ARTICLE API！！！');
-// })
-
-
-// app.use('/api/v1/user', userRouter);
-
-
-// //jientin
-// app.listen(process.env.APP_PORT, function() {
-// 	console.log("伺服器已經啟動在PORT: ",process.env.APP_PORT);
-// }); 
-
+//define root route
+app.get('/',(req,res) => {
+  res.status(200).send('WELCOME TO ARTICLE API！！！');
+})
 
 app.use('/api/v1/article', articleRoutes);
 app.use("/api/v1/user", userRouter);
+
+// //jientin
 app.listen(process.env.APP_PORT, () => {
   console.log("server up and running on PORT :", process.env.APP_PORT);
 });
